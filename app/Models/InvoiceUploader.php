8f7370dc409cab61c0c0854c8ee27a854a820ad1
Benjamin Brant <benjamin661@live.co.uk>
2023-03-15 12:27:56 +0000

    Uploads using job and invoice model.

--- app/Models/InvoiceUploader.php
+++ app/Models/InvoiceUploader.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Job;
 use App\Models\Invoice;
@@ -49,6 +50,42 @@ class InvoiceUploader
     }
 
     /**
+     * Generate new job and create model entries for files pending upload
+     * @return int
+     */
+    public function generateModelPayload()
+    {
+        //generate new job
+        $job = new Job(["errors_encountered" => false]);
+        $job->save();
+        $jobId = $job->id;
+
+        //Obtain a list of files to process from the storage/app/invoices/outbound mounted samba share folder
+        $files = Storage::disk('outbound')->files();
+
+        //Create a payload entry from each filename
+        foreach ($files as $file)
+        {
+            //Obtain PO number from filename and add to payload as key
+            $components = explode('_', $file);
+            $po = trim($components[0]);
+
+            //Create and save model
+            $invoice = new Invoice([
+                "filename" => $file,
+                "po" => $po,
+                "local_size" => Storage::disk('outbound')->size($file),
+                "is_uploaded" => false,
+                "is_processed" => false,
+            ]);
+
+            $job->invoices()->save($invoice);
+        }
+
+        return $jobId;
+    }
+
+    /**
      * Take payload array and attempt to upload files to sftp endpoint
      * record any errors and move files accordingly
      * @param array $payload
@@ -85,6 +122,106 @@ class InvoiceUploader
         return $payload;
     }
 
+    public function processModelPayload(int $jobId)
+    {
+        //attempt to pull job model
+        $job = $this->pullJobRecord($jobId);
+
+        if ($job)
+        {
+            foreach ($job->invoices()->getResults() as $invoice)
+            {
+                if ($this->uploadFile($invoice->filename))
+                {
+                    //file uploaded
+                    $invoice->remote_size = Storage::disk('sftp')->size($invoice->filename);
+                    $invoice->is_uploaded = true;
+                    $invoice->is_identical_filesize = ($invoice->local_size === $invoice->remote_size);
+                }
+                else
+                {
+                    //file failed upload
+                    $invoice->is_uploaded = false;
+                    //indicate errors encountered on main job
+                    $job->errors_encountered = true;
+                    $job->save();
+                }
+
+                //save model back to db
+                $invoice->save();
+            }
+            return true;
+        }
+        else
+        {
+            //job model not found in db
+            return false;
+        }
+    }
+
+    public function archiveUploadedFiles(int $jobId)
+    {
+        $job = $this->pullJobRecord($jobId);
+
+        if ($job)
+        {
+            //@todo set archive storage path
+
+            foreach ($job->invoices()->getResults() as $invoice)
+            {
+                if ($invoice->is_uploaded)
+                {
+                    //archive
+                }
+                else
+                {
+                    //move to error folder
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Utility Functions
+
+    /**
+     * Upload file to SFTP endpoint
+     * @param string $filename
+     * @return bool
+     */
+    protected function uploadFile(string $filename): bool
+    {
+        return Storage::disk('sftp')
+            ->put(
+                $filename,
+                Storage::disk('outbound')->get($filename)
+            );
+    }
+
+    /**
+     * Pull a job record back if found
+     * @param int $jobId
+     * @return Job | false
+     */
+    protected function pullJobRecord(int $jobId): Job | false
+    {
+        try {
+            return Job::findOrFail($jobId);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return false;
+        }
+    }
+
+    protected function setArchiveSToragePath()
+    {
+        //@todo
+    }
+
     //Scratch functions
     public function test()
     {
@@ -105,4 +242,6 @@ class InvoiceUploader
     {
         $job = new Job;
     }
+
+
 }
